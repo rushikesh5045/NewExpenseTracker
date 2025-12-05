5379ffd149f4c20661bf43b40e22fa1e3c6b94ae
rushi <rushikesh.k21@iiits.in>
2025-12-06 00:20:09 +0530

    chore: use deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5004/api";
+const API_URL = "https://newexpensetracker-yhwf.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
